d0a95bb612f7b06b6509d74da13da34e0d38c784
@yurisa2 <yuri@sa2.com.br>
2022-09-15 14:21:57 -0300

    Logging Better

--- app/bin_data.py
+++ app/bin_data.py
@@ -17,34 +17,39 @@ def get_data_bin(symbol,
               }
               )
 
-    df = pd.DataFrame()
+    try:
+        df = pd.DataFrame()
 
-    lticker, ldatetime, lopen, lhigh, llow, lclose, lclose_time, lclosed_candle, lqty, lvol, = [
-        ], [], [], [], [], [], [], [], [], []
+        lticker, ldatetime, lopen, lhigh, llow, lclose, lclose_time, lclosed_candle, lqty, lvol, = [
+            ], [], [], [], [], [], [], [], [], []
 
-    for candle in result.json():
-        lticker.append(symbol)
-        ldatetime.append(candle[0])
-        lopen.append(float(candle[1]))
-        lhigh.append(float(candle[2]))
-        llow.append(float(candle[3]))
-        lclose.append(float(candle[4]))
-        lvol.append(float(candle[5]))
-        lclose_time.append(candle[6])
-        lqty.append(float(candle[8]))
-        lclosed_candle.append(True)
+        for candle in result.json():
+            lticker.append(symbol)
+            ldatetime.append(candle[0])
+            lopen.append(float(candle[1]))
+            lhigh.append(float(candle[2]))
+            llow.append(float(candle[3]))
+            lclose.append(float(candle[4]))
+            lvol.append(float(candle[5]))
+            lclose_time.append(candle[6])
+            lqty.append(float(candle[8]))
+            lclosed_candle.append(True)
 
-    df['s'] = lticker
-    df['t'] = ldatetime
-    df['o'] = lopen
-    df['h'] = lhigh
-    df['l'] = llow
-    df['c'] = lclose
-    df['T'] = lclose_time
-    df['x'] = lclosed_candle
-    df['n'] = lqty
-    df['n'] = lvol
+        df['s'] = lticker
+        df['t'] = ldatetime
+        df['o'] = lopen
+        df['h'] = lhigh
+        df['l'] = llow
+        df['c'] = lclose
+        df['T'] = lclose_time
+        df['x'] = lclosed_candle
+        df['n'] = lqty
+        df['n'] = lvol
 
-    # df = df.set_index('datetime')
+        # df = df.set_index('datetime')
+    except Exception as e:
+        print(e)
+        print(result.json())
+        raise
 
     return df
